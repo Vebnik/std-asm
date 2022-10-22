23ab02d72cbaffc2d6b35bc028925134c7170a7b
Vebnik(Nik) <vebnik111@gmail.com>
2022-10-22 05:42:51 +0300

    init rep

---
+++ hw.cpp
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  char name[8] = "Test";
+
+  int num1 = 120;
+
+  int num2 = 120;
+
+  printf("Hello World!\nNice Programm %s\n", name);
+
+  return 0;
+}
